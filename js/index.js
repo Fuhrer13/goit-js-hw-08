1a1e28b8d549b723eff5ac6fe5ad30769510eb85
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-02-27 23:29:37 +0200

    Done

---
+++ js/index.js
@@ -0,0 +1,48 @@
+import gallery from './gallery-items.js';
+
+const galleryUl = document.querySelector('.js-gallery');
+
+const createGallery = function(array) {
+  const arrReduce = array.reduce(
+    (acc, item) =>
+      (acc += item = `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href=${item.original}
+    >
+      <img
+        class="gallery__image"
+        src=${item.preview}
+        data-source=${item.original}
+        alt=${item.description}
+      />
+    </a>
+  </li>`),
+    '',
+  );
+  return arrReduce;
+};
+const itemGallery = createGallery(gallery);
+galleryUl.insertAdjacentHTML('beforeend', itemGallery);
+
+const modalWindow = document.querySelector('.js-lightbox');
+const modalImage = document.querySelector('.lightbox__image');
+const openModalWindow = e => {
+  e.preventDefault();
+  const targetImg = e.target;
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  modalWindow.classList.add('is-open');
+  modalImage.src = targetImg.dataset.source;
+};
+
+galleryUl.addEventListener('click', openModalWindow);
+
+const closeBttn = modalWindow.querySelector('.lightbox__button');
+const closeModalWindow = e => {
+  e.preventDefault();
+  modalWindow.classList.remove('is-open');
+  modalImage.src = '';
+};
+closeBttn.addEventListener('click', closeModalWindow);
